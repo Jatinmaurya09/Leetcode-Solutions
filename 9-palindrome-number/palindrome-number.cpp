9d054f454410bb44950a6983e8d6bca909ed7ed4
Jatin Maurya <jatinkmaurya09@gmail.com>
2026-09-05 17:32:28 +0530

    Time: 3 ms (43.90%) | Memory: 8.6 MB (35.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         int duplicate=x;
-        long ans=0;
+        long long ans=0;
         while(x>0){
             int last_digit=x%10;
-            ans=ans*10+last_digit;
             x/=10;
+            ans=ans*10+last_digit;
         }
         if(ans==duplicate) return true;
-        return false;
+        else return false;
     }
 };
